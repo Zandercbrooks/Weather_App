2b575f4191b689597fe8811adb0dafa8a5dce375
Zandermelon <Zandermelon428@gmail.com>
2025-08-11 23:28:43 -0700

    grabbed temperature from json file

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import requests
 
-cityName = ""
-apiKey = ""
-url = ""
+cityName = "Los Angeles"
+apiKey = "0c9ddd24c12ae2e9e8a03d419b946243"
 weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric")
+
+if weatherData.status_code == 200:
+    print(weatherData.json())
+    temp = weatherData["main"]["temp"]
+    
