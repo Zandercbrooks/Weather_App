4a83df40324a9603c70eb7a88fef12dfc6f689a3
Zandermelon <Zandermelon428@gmail.com>
2025-08-12 02:23:00 -0700

    moved get weather into app

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import requests
-from app import App
+from app import App, WeatherAPI
 
-
-
-
-
-def getWeather():
-    cityName = app.getCity()
-    apiKey = "0c9ddd24c12ae2e9e8a03d419b946243"
-    weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric")
-
-    if weatherData.status_code == 200:
-        data = weatherData.json()
-        # Pulling data from weatherData
-        temp = data["main"]["temp"]
-        print(f"Temperature in {cityName}: {temp}°C")
-    else:
-        print("City not found")
-
-# run app
-app = App(on_enter=getWeather)
-app.mainloop()
+if __name__ == "__main__":
+    api = WeatherAPI(api_key="0c9ddd24c12ae2e9e8a03d419b946243")
+    app = App(api)
+    app.mainloop()
