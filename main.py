4749470258512fd68f208f45eaf72d26cf1a050e
Zandermelon <Zandermelon428@gmail.com>
2025-08-12 00:53:24 -0700

    moved running app code to top

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import requests
 from app import App
+
+# run app
+app = App()
+app.mainloop()
+
 cityName = "Los Angeles"
 apiKey = "0c9ddd24c12ae2e9e8a03d419b946243"
 weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={cityName}&appid={apiKey}&units=metric")
@@ -10,6 +15,3 @@ weatherData = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={
     #temp = data["main"]["temp"]
 
 
-# run app
-app = App()
-app.mainloop()
